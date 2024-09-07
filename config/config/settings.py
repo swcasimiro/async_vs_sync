2558a10294f4f46c616782e8c6778c5a162d8af0
swcasimiro <comradecasi@gmail.com>
2024-09-07 17:42:09 +0300

    Update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -14,16 +14,18 @@ from pathlib import Path
 import os
 
 from django.conf.global_settings import INTERNAL_IPS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f0cf(+7!ja86mvpmgb8&&u!f+zf^ejpkax*1jtrf3#wk=_9g$3'
+SECRET_KEY = os.gotenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
